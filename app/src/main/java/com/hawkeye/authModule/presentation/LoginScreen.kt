17f5d6f48ee36bfbc8631e83aa78b607889461e6
Shreyas KB <shreyaskb333@gmail.com>
2023-07-04 20:53:20 +0530

    Redirection button

--- app/src/main/java/com/hawkeye/authModule/presentation/LoginScreen.kt
+++ app/src/main/java/com/hawkeye/authModule/presentation/LoginScreen.kt
@@ -2,8 +2,15 @@ package com.hawkeye.authModule.presentation
 
 import android.widget.Toast
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -15,8 +22,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -24,9 +31,17 @@ import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.hawkeye.authModule.domain.repository.ErrorHolder
-import com.hawkeye.authModule.presentation.components.*
+import com.hawkeye.authModule.presentation.components.AuthButton
+import com.hawkeye.authModule.presentation.components.BubbleAnimation
+import com.hawkeye.authModule.presentation.components.HeaderBackground
+import com.hawkeye.authModule.presentation.components.NavDestinationHelper
+import com.hawkeye.authModule.presentation.components.TextEntryModule
 import com.hawkeye.authModule.presentation.viewmodel.LoginViewModel
-import com.hawkeye.ui.theme.*
+import com.hawkeye.ui.theme.gray
+import com.hawkeye.ui.theme.orange
+import com.hawkeye.ui.theme.white
+import com.hawkeye.ui.theme.whiteGray
+import com.hawkeye.ui.theme.whiteGrayOrange
 
 @Composable
 fun LoginScreen(
@@ -151,7 +166,9 @@ fun LoginContainer(
     isLoading:()->Boolean,
     modifier: Modifier = Modifier
 ) {
-   Column(
+    val focusManager = LocalFocusManager.current
+
+    Column(
        modifier = modifier,
        verticalArrangement = Arrangement.spacedBy(15.dp)
    ){
